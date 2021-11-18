fff64135a62698950977caadf6db515154cc192a
KaczmarekWojciech <89963232+KaczmarekWojciech@users.noreply.github.com>
2021-11-18 08:40:25 +0100

    Add files via upload

--- pracowniaprogramowania/4B/7_bazy_danych/scripts/adduser.php
+++ pracowniaprogramowania/4B/7_bazy_danych/scripts/adduser.php
@@ -10,14 +10,14 @@
     $sql="INSERT INTO `users` (`id`, `cityid`, `name`, `surname`, `birthday`) VALUES (NULL, '$_POST[cityid]', '$_POST[name]', '$_POST[surname]', '$_POST[birthday]');";
     $connect->query($sql);
     if ($connect->affected_rows == 1) {
-      header("location: ../4_bazy_tabela_insert.php?error=2&info=Prawidłowo dodano użytkownika!");
+      header("location: ../5_bazy_tabela_update.php?error=2&info=Prawidłowo dodano użytkownika!");
     }
     else {
-      header("location: ../4_bazy_tabela_insert.php?error=2&info=Nie dodano użytkownika!&addUser=");
+      header("location: ../5_bazy_tabela_update.php?error=2&info=Nie dodano użytkownika!&addUser=");
     }
     $connect->close();
   }
   else{
-    header("location: ../4_bazy_tabela_insert.php?error=2&info=Wypełnij wszystkie dane!&addUser=");
+    header("location: ../5_bazy_tabela_update.php?error=2&info=Wypełnij wszystkie dane!&addUser=");
   }
 ?>
